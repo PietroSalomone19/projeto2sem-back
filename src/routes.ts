a860102aa152f4eb16032936115fa7d222ecd255
Pietro Almeida <pietrosalomone11@gmail.com>
2024-10-01 13:26:35 -0300

    criando repositorio

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router} from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+    res.json({ mensagem: "Olá mundo!"});
+});
+
+export {router};
